095da3b15295405601776345c06a52cded310b6d
Vaskulis <vaskulis@posteo.de>
2021-05-30 10:58:33 +0200

    changed so2sat pipeline to actually use distributed bm

--- pipelines/so2sat_pipeline/benchmarking.py
+++ pipelines/so2sat_pipeline/benchmarking.py
@@ -3,4 +3,4 @@ import sys
 
 import e2ebench
 
-bm = e2ebench.Benchmark('so2sat_benchmark.db', description="So2Sat image prediction benchmark")
+bm = e2ebench.DistributedBenchmark('so2sat_benchmark.db', description="So2Sat image prediction benchmark")
