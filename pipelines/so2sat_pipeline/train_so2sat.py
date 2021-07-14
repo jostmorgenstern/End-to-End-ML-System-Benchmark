37a6580e890b200d903ff073d5fc77f6668be55f
Vaskulis <vaskulis@posteo.de>
2021-07-14 09:45:30 +0200

    changed so2sat pipeline to use tf datasets

--- pipelines/so2sat_pipeline/train_so2sat.py
+++ pipelines/so2sat_pipeline/train_so2sat.py
@@ -8,17 +8,43 @@ from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 
 
-def load_data(num_samples=None):
-    f = h5py.File('data/training.h5', 'r')
-    n = num_samples or len(f['label'])  # if num_samples is 0 or None, use all samples
-    input_train = f['sen1'][0:n]
-    label_train = f['label'][0:n]
-    f.close()
-    f = h5py.File('data/validation.h5', 'r')
-    input_val = f['sen1'][0:len(f['label'])]
-    label_val = f['label'][0:len(f['label'])]
-    f.close()
-    return input_train, label_train, input_val, label_val, n
+class DatasetGenerator:
+    def __init__(self, file):
+        self.file = file
+
+    def __call__(self, *args, **kwargs):
+        with h5py.File(self.file,  'r') as f:
+            for i in range(len(f['label'])):
+                yield f['sen1'][i], f['label'][i]
+
+
+# def load_data(num_samples=None):
+#     f = h5py.File('data/training.h5', 'r')
+#     n = num_samples or len(f['label'])  # if num_samples is 0 or None, use all samples
+#     input_train = f['sen1'][0:n]
+#     label_train = f['label'][0:n]
+#     f.close()
+#     f = h5py.File('data/validation.h5', 'r')
+#     input_val = f['sen1'][0:len(f['label'])]
+#     label_val = f['label'][0:len(f['label'])]
+#     f.close()
+#     return input_train, label_train, input_val, label_val, n
+
+
+def load_data():
+    train_ds = tf.data.Dataset.from_generator(DatasetGenerator('training.h5'),
+                                              output_signature=(tf.TensorSpec(shape=(32, 32, 8),
+                                                                              dtype=tf.float64),
+                                                                tf.TensorSpec(shape=17,
+                                                                              dtype=tf.int8)))
+
+    validation_ds = tf.data.Dataset.from_generator(DatasetGenerator('validation.h5'),
+                                                   output_signature=(tf.TensorSpec(shape=(32, 32, 8),
+                                                                                   dtype=tf.float64),
+                                                                     tf.TensorSpec(shape=17,
+                                                                                   dtype=tf.int8)))
+
+    return train_ds, validation_ds
 
 
 def compile_model(input_shape, num_classes, loss_function, optimizer):
@@ -46,40 +72,42 @@ latency_metric = eb.LatencyMetric('train latency')
 
 @eb.BenchmarkSupervisor([eb.MemoryMetric('train memory'),
                          eb.TimeMetric('train time'),
-                         eb.CPUMetric('train cpu usage'),
-                         throughput_metric,
-                         latency_metric
+                         eb.CPUMetric('train cpu usage')
+                         # throughput_metric,
+                         # latency_metric
                          ], bm)
 def train():
-    batch_size = 256
-    img_width, img_height, img_num_channels = 32, 32, 8
-    input_shape = (img_width, img_height, img_num_channels)
+    per_worker_batch_size = 128
+    input_shape = (32, 32, 8)
     loss_function = "categorical_crossentropy"
     num_classes = 17
     num_epochs = 10
     optimizer = Adam()
     verbosity = 1
 
-    input_train, label_train, input_val, label_val, num_samples = load_data()
-
-    input_train = input_train.reshape((len(input_train), img_width, img_height, img_num_channels))
-    input_val = input_val.reshape((len(input_val), img_width, img_height, img_num_channels))
+    # input_train, label_train, input_val, label_val, num_samples = load_data()
+    #
+    # input_train = input_train.reshape((len(input_train), img_width, img_height, img_num_channels))
+    # input_val = input_val.reshape((len(input_val), img_width, img_height, img_num_channels))
 
     strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
-    num_workers = strategy.num_replicas_in_sync
-
     with strategy.scope():
+        global_batch_size = per_worker_batch_size * strategy.num_replicas_in_sync
+
+        train_ds, val_ds = load_data()
+        dist_train_ds = strategy.experimental_distribute_dataset(train_ds).batch(global_batch_size)
+        # dist_val_ds = strategy.experimental_distribute_dataset(val_ds)
+
         model = compile_model(input_shape, num_classes, loss_function, optimizer)
 
-    history = model.fit(input_train, label_train,
-                        batch_size=batch_size,
-                        epochs=num_epochs,
-                        verbose=verbosity,
-                        validation_data=(input_val, label_val))
+        history = model.fit(dist_train_ds,
+                            epochs=num_epochs,
+                            verbose=verbosity,
+                            validation_data=val_ds)
 
-    throughput_metric.track(num_samples / num_workers)
-    latency_metric.track(num_samples / num_workers)
+    # throughput_metric.track(num_samples / num_workers)
+    # latency_metric.track(num_samples / num_workers)
 
-    return {"model": model, "num_entries": len(input_train), "classifier": optimizer,
+    return {"model": model, "classifier": optimizer,
             "accuracy": history.history["accuracy"]}
