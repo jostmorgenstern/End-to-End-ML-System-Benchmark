4bdf2e2b62041bd11163c36d9c7a9ebf1e0b2fec
Vaskulis <vaskulis@posteo.de>
2021-07-15 14:09:28 +0200

    another try using a generator with batching

--- pipelines/so2sat_pipeline/train_so2sat.py
+++ pipelines/so2sat_pipeline/train_so2sat.py
@@ -9,15 +9,21 @@ import tensorflow as tf
 import numpy as np
 
 
-# class DatasetGenerator:
-#     def __init__(self, file):
-#         self.file = file
-#
-#     def __call__(self, *args, **kwargs):
-#         with h5py.File(self.file,  'r') as f:
-#             # for i in range(len(f['label'])):
-#             for i in range(4096):
-#                 yield f['sen1'][i], f['label'][i]
+class DatasetGenerator:
+    def __init__(self, file, global_batch_size):
+        self.file = file
+        self.global_batch_size = global_batch_size
+
+    def __call__(self, *args, **kwargs):
+        with h5py.File(self.file, 'r') as f:
+            file_len = len(f['label'])
+            start_index = 0
+            end_index = self.global_batch_size - 1
+            while start_index < file_len:
+                yield (np.concatenate((f['sen1'][start_index:end_index], f['sen2'][start_index:end_index]), axis=3),
+                       f['label'][start_index:end_index])
+                start_index += self.global_batch_size
+                end_index += self.global_batch_size
 
 
 def load_data(num_samples=None):
@@ -29,22 +35,6 @@ def load_data(num_samples=None):
         input_val = np.concatenate((val_f['sen1'][0:n], val_f['sen2'][0:n]), axis=3)
         label_val = val_f['label'][0:len(val_f['label'])]
     return input_train, label_train, input_val, label_val, n
-
-
-# def load_data():
-#     train_ds = tf.data.Dataset.from_generator(DatasetGenerator('data/training.h5'),
-#                                               output_signature=(tf.TensorSpec(shape=(32, 32, 8),
-#                                                                               dtype=tf.float64),
-#                                                                 tf.TensorSpec(shape=17,
-#                                                                               dtype=tf.float64)))
-#
-#     validation_ds = tf.data.Dataset.from_generator(DatasetGenerator('data/validation.h5'),
-#                                                    output_signature=(tf.TensorSpec(shape=(32, 32, 8),
-#                                                                                    dtype=tf.float64),
-#                                                                      tf.TensorSpec(shape=17,
-#                                                                                    dtype=tf.float64)))
-#
-#     return train_ds, validation_ds
 
 
 def compile_model(input_shape, num_classes, loss_function, optimizer):
@@ -81,14 +71,9 @@ def train():
     input_shape = (32, 32, 18)
     loss_function = "categorical_crossentropy"
     num_classes = 17
-    num_epochs = 1
+    num_epochs = 10
     optimizer = Adam()
     verbosity = 1
-
-    # input_train, label_train, input_val, label_val, num_samples = load_data()
-    #
-    # input_train = input_train.reshape((len(input_train), img_width, img_height, img_num_channels))
-    # input_val = input_val.reshape((len(input_val), img_width, img_height, img_num_channels))
 
     strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
@@ -97,12 +82,23 @@ def train():
 
         global_batch_size = per_worker_batch_size * num_workers
 
-        input_train, label_train, input_val, label_val, num_samples = load_data(num_samples=32768)
-        train_ds = tf.data.Dataset.from_tensor_slices((input_train, label_train)).batch(global_batch_size)
-        val_ds = tf.data.Dataset.from_tensor_slices((input_val, label_val)).batch(global_batch_size)
+        # input_train, label_train, input_val, label_val, num_samples = load_data(num_samples=32768)
+        # train_ds = tf.data.Dataset.from_tensor_slices((input_train, label_train)).batch(global_batch_size)
+        # val_ds = tf.data.Dataset.from_tensor_slices((input_val, label_val)).batch(global_batch_size)
 
         # train_ds.prefetch(3)
         # val_ds.prefetch(3)
+
+        train_ds = tf.data.Dataset.from_generator(DatasetGenerator('training.h5', global_batch_size),
+                                                  output_signature=(tf.TensorSpec(shape=(32, 32, 18),
+                                                                                  dtype=tf.float64),
+                                                                    tf.TensorSpec(shape=17,
+                                                                                  dtype=tf.int8)))
+        val_ds = tf.data.Dataset.from_generator(DatasetGenerator('validation.h5', global_batch_size),
+                                                output_signature=(tf.TensorSpec(shape=(32, 32, 18),
+                                                                                dtype=tf.float64),
+                                                                  tf.TensorSpec(shape=17,
+                                                                                dtype=tf.int8)))
 
         model = compile_model(input_shape, num_classes, loss_function, optimizer)
 
@@ -110,6 +106,8 @@ def train():
                             epochs=num_epochs,
                             verbose=verbosity,
                             validation_data=val_ds)
+
+    num_samples = 3  # bullshit
 
     throughput_metric.track((num_samples / num_workers) * num_epochs)
     latency_metric.track((num_samples / num_workers) * num_epochs)
